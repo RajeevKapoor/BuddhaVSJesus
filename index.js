e318b1419ae23f3fbc13509b753a5b87da05348f
severepain <47048628+SeverePain@users.noreply.github.com>
2020-02-25 16:28:12 -0800

    Added Timers (You can only use one of the bots every 2 seconds)

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const cleverbot = require("cleverbot-free");
 const jesus_client = new Discord.Client();
 const buddha_client = new Discord.Client();
 
+const timerSet = new Set();
+
 jesus_client.on('ready', () => {
     console.log(`Logged in as ${jesus_client.user.tag}!`);
     jesus_client.channels.get(`681970286597373966`).send(`<@${buddha_client.user.id}> Hi!`)
@@ -23,6 +25,15 @@ buddha_client.on('ready', () => {
 });
 
 jesus_client.on('message', async message => {
+    if(!timerSet.has(message.author.id) && message.mentions.users.first() === jesus_client.user){
+ 	timerSet.add(message.author.id)
+	setTimeout(function(){
+		timerSet.delete(message.author.id)
+	}, 2 * 1000)
+    }
+    else if(message.mentions.users.first() === jesus_client.user){
+	return message.reply(`:no_entry: You must wait 2 seconds before talking to the bot again!`)
+	}
     if (message.mentions.users.first() === jesus_client.user) {
         await message.channel.startTyping()
         await cleverbot(message.content.replace(`<@${jesus_client.user.id}> `, ``).replace(`<@!${jesus_client.user.id}> `, ``)).then(async response => {
@@ -32,6 +43,16 @@ jesus_client.on('message', async message => {
     }
 });
 buddha_client.on('message', async message => {
+    if(!timerSet.has(message.author.id) && message.mentions.users.first() === buddha_client.user){
+ 	timerSet.add(message.author.id)
+	setTimeout(function(){
+		timerSet.delete(message.author.id)
+	}, 2 * 1000)
+    }
+    else if(message.mentions.users.first() === buddha_client.user) {
+	return message.reply(`:no_entry: You must wait 2 seconds before talking to the bot again!`)
+	}
+
     if (message.mentions.users.first() === buddha_client.user) {
         await message.channel.startTyping()
         await cleverbot(message.content.replace(`<@${buddha_client.user.id}> `, ``).replace(`<@!${buddha_client.user.id}> `, ``)).then(async response => {
