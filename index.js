fd046b61632f60ba99d01ee7ba7e4b46596ba403
Rajeev Kapoor <44755991+RajeevKapoor@users.noreply.github.com>
2020-02-27 10:06:27 +0530

    fucks sake

--- index.js
+++ index.js
@@ -7,17 +7,17 @@ const client2 = new Discord.Client();
 const timerSet = new Set();
 
 client1.on('ready', () => {
-    console.log(`Logged in as ${jesus_client.user.tag}!`);
-    jesus_client.channels.get(config.aichannel).send(`<@${buddha_client.user.id}> Hi!`)
-    jesus_client.channels.forEach(channel => {
+    console.log(`Logged in as ${client1.user.tag}!`);
+    client1.channels.get(config.aichannel).send(`<@${client2.user.id}> Hi!`)
+    client1.channels.forEach(channel => {
 		if(channel.type == "text"){
 			channel.stopTyping()
 		}
 	})
 });
 client2.on('ready', () => {
-    console.log(`Logged in as ${buddha_client.user.tag}!`);
-    buddha_client.channels.forEach(channel => {
+    console.log(`Logged in as ${client2.user.tag}!`);
+    client2.channels.forEach(channel => {
 		if(channel.type == "text"){
 			channel.stopTyping()
 		}
@@ -25,37 +25,37 @@ client2.on('ready', () => {
 });
 
 client1.on('message', async message => {
-    if(!timerSet.has(message.author.id) && message.mentions.users.first() === jesus_client.user &&! message.author.bot){
+    if(!timerSet.has(message.author.id) && message.mentions.users.first() === client1.user &&! message.author.bot){
  	timerSet.add(message.author.id)
 	setTimeout(function(){
 		timerSet.delete(message.author.id)
 	}, 2 * 1000)
     }
-    else if(message.mentions.users.first() === jesus_client.user &&! message.author.bot){
+    else if(message.mentions.users.first() === client1.user &&! message.author.bot){
 	return message.reply(`:no_entry: You must wait 2 seconds before talking to the bot again!`)
 	}
-    if (message.mentions.users.first() === jesus_client.user) {
+    if (message.mentions.users.first() === client1.user) {
         await message.channel.startTyping()
-        await cleverbot(message.content.replace(`<@${jesus_client.user.id}> `, ``).replace(`<@!${jesus_client.user.id}> `, ``)).then(async response => {
+        await cleverbot(message.content.replace(`<@${client1.user.id}> `, ``).replace(`<@!${client1.user.id}> `, ``)).then(async response => {
             await message.reply(response)
             await message.channel.stopTyping()
         });
     }
 });
 client2.on('message', async message => {
-    if(!timerSet.has(message.author.id) && message.mentions.users.first() === buddha_client.user &&! message.author.bot){
+    if(!timerSet.has(message.author.id) && message.mentions.users.first() === client2.user &&! message.author.bot){
  	timerSet.add(message.author.id)
 	setTimeout(function(){
 		timerSet.delete(message.author.id)
 	}, 2 * 1000)
     }
-    else if(message.mentions.users.first() === buddha_client.user &&! message.author.bot) {
+    else if(message.mentions.users.first() === client2.user &&! message.author.bot) {
 	return message.reply(`:no_entry: You must wait 2 seconds before talking to the bot again!`)
 	}
 
-    if (message.mentions.users.first() === buddha_client.user) {
+    if (message.mentions.users.first() === client2.user) {
         await message.channel.startTyping()
-        await cleverbot(message.content.replace(`<@${buddha_client.user.id}> `, ``).replace(`<@!${buddha_client.user.id}> `, ``)).then(async response => {
+        await cleverbot(message.content.replace(`<@${client2.user.id}> `, ``).replace(`<@!${client2.user.id}> `, ``)).then(async response => {
             await message.reply(response)
             await message.channel.stopTyping()
         });
@@ -63,5 +63,5 @@ client2.on('message', async message => {
 });
 
 // Login to the bots
-client1.login(config.buddha_token);
-client2.login(config.jesus_token);
+client1.login(config.client1_token);
+client2.login(config.client2_token);
